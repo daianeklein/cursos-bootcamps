673f42d1746c5e2c0a4ed4a8eb580a762ccae8d5
daianeklein <daiane.klein22@gmail.com>
2021-02-14 15:52:35 -0300

    finalizacao curso

--- MLOPs/MLOPs-01/main.py
+++ MLOPs/MLOPs-01/main.py
@@ -1,17 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from textblob import TextBlob
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-app = Flask('meu_app')
+df = pd.read_csv('casas.csv')
+colunas = ['tamanho', 'ano', 'garagem']
+
+
+X = df.drop('preco', axis=1)
+y = df['preco']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Minha Primeira API."
+    return "Minha primeira API."
 
 @app.route('/sentimento/<frase>')
-def sentimentos(frase):
-	tb = TextBlob(frase)
-	tb_en = tb.translate(to='en')
-	polaridade = tb_en.sentiment.polarity
-	return "polaridade: {}".format(polaridade)
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(to='en')
+    polaridade = tb_en.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+@app.route('/cotacao/', methods=['POST'])
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])
 
 app.run(debug=True)
--- MLOPs/MLOPs-01/main_S.py
+++ MLOPs/MLOPs-01/main_S.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
 from textblob import TextBlob
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -8,12 +9,18 @@ colunas = ['tamanho', 'ano', 'garagem']
 modelo = pickle.load(open('modelo.sav', 'rb'))
 
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'Daiane'
+app.config['BASIC_AUTH_PASSWORD'] = 'senha'
+
+
+basic_auth = BasicAuth(app)
 
 @app.route('/')
 def home():
     return "Minha primeira API."
 
 @app.route('/sentimento/<frase>')
+@basic_auth.required
 def sentimento(frase):
     tb = TextBlob(frase)
     tb_en = tb.translate(to='en')
@@ -21,6 +28,7 @@ def sentimento(frase):
     return "polaridade: {}".format(polaridade)
 
 @app.route('/cotacao/', methods=['POST'])
+@basic_auth.required
 def cotacao():
     dados = request.get_json()
     dados_input = [dados[col] for col in colunas]
