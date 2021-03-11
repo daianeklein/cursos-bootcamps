a762a8ac3e48dbe706e8f96d01298f48eac380a1
daianeklein <daiane.klein22@gmail.com>
2021-03-10 21:48:08 -0300

    add curso R

---
+++ R/Analise de dados - Introducao com R/R_Introducao.R
@@ -0,0 +1,35 @@
+# "mostrando" qual eh o arquivo
+attach(aulas)
+
+# Selecionando a variavel section_id do arquivo aulas
+section_id
+
+# Alterando a quantidade de dados "printados
+options(max.print = 40000)
+
+# Visualizando a variavel section_id com 40k de registros configurados
+section_id
+
+# Mostrando os primeiros registros
+head(section_id)
+
+# Ordenando do menor para o maior
+sort(section_id)
+
+#Substituindo um valor
+# 33137 == a linha que o dado se encontra
+# 3 == a coluna que o dado se encontra
+aulas[33137, 3] <- 3255
+
+# viz o arquivo (e nao a variavel armazenada)
+sort(aulas$section_id)
+
+# Executando somente um dado especifico
+aulas[33137, 3]
+
+# Visualizando os valores unicos
+unique(aulas$section_id)
+
+# Tamanho do dataset
+# nesting (aninhamento) == funcao dentro de outra funcao
+length(unique(aulas$section_id))
