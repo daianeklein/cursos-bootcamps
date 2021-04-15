eaabcda5b06b536e8672676d0bc7e9924486bc51
Daiane Klein <65792911+daianeklein@users.noreply.github.com>
2021-04-14 21:40:19 -0300

    Update aula 06

--- R/Aula06 - Graficos Estatisticos para Variaveis Qualitativas Nominais.R
+++ R/Aula06 - Graficos Estatisticos para Variaveis Qualitativas Nominais.R
@@ -3,6 +3,7 @@ install.packages(c("scales", "tidyverse"))
 require(ggplot2)
 require(dplyr)
 require(scales)
+require(gridExtra)
 
 install.packages("plotly")
 library(plotly)
@@ -55,3 +56,50 @@ class(grafico_coluna_geral)
 
 # Deixando grafico interativo
 ggplotly(grafico_coluna_geral)
+
+#graficos de colunas ou barras verticais por sexo
+grafico_coluna=ggplot(variaveis_categoricas_nominais,aes(a_cor_olhos,fill=a_sexo)) + 
+  geom_bar(position = "dodge") +
+  ggtitle("Número de alunos por cor dos olhos e sexo")+
+  labs(fill="Sexo")+
+  xlab("Cor dos olhos") +
+  ylab("Frequência simples (Quantidade de alunos)") 
+
+ggplotly(grafico_coluna)
+
+
+
+#Gráfico de Colunas ou barras verticais (%)
+grafico_coluna_porcent=ggplot(variaveis_categoricas_nominais,aes(a_cor_olhos,fill=a_sexo)) + 
+  geom_bar(position = "dodge",aes(y = (..count..)/sum(..count..))) +
+  ggtitle("Número de alunos por cor dos olhos e sexo (%)")+
+  scale_y_continuous(labels=percent)+
+  labs(fill="Sexo")+
+  xlab("Cor dos olhos") +
+  ylab("Frequência simples (Quantidade de alunos)")
+
+ggplotly(grafico_coluna_porcent)
+
+#grafico de colunas ou barras verticais empilhadas (%)
+graficos_colunas_empilhadas=ggplot(variaveis_categoricas_nominais, aes(a_cor_olhos, fill=a_sexo)) +
+  geom_bar(position = 'fill') +
+  ggtitle('Numero de alunos por cor dos olhos e sexo') +
+  scale_y_continuous(label = percent) +
+  labs(fill='sexo') +
+  xlab('cor dos olhos') +
+  ylab('Frequencia Simples (Qt. alunos)')
+
+ggplotly(graficos_colunas_empilhadas)
+
+#Grafico de barras horizontais
+grafico_barras_horizontais_por_sexo= grafico_coluna + coord_flip()
+ggplotly(grafico_barras_horizontais)
+
+#varios graficos
+grid.arrange(grafico_coluna_geral,
+             grafico_coluna,
+             grafico_coluna_porcent,
+             grafico_barras_horizontais,
+             grafico_barras_horizontais_por_sexo,
+             graficos_colunas_empilhadas,
+             nrow = 3, ncol=2)
