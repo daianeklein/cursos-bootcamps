07ec26ac9209f14a930075cd4ff9b4dd761144c9
Daiane Klein <65792911+daianeklein@users.noreply.github.com>
2021-06-06 11:58:17 -0300

    update ex 03

--- Mackenzie/Ex03.R
+++ Mackenzie/Ex03.R
@@ -56,3 +56,55 @@ round(e1,3)
 plot(mod1$fitted,e1, pch=16)
 abline(h=0, col=2, lty=2) 
 
+#mod 02
+head(df)
+
+df$Lprice =log(df$price)
+
+hist(df$Lprice)
+hist(df$price)
+
+mod02 = lm(Lprice~horsepower, data = df)
+summary(mod02)
+
+plot(df$horsepower, df$Lprice)
+abline(coef(mod02))
+
+#residual
+e1 <- resid(mod02)
+round(e1,3) 
+plot(mod1$fitted,e1, pch=16)
+abline(h=0, col=2, lty=2) 
+
+#mod03
+df$Lhorsepower = log(df$horsepower)
+hist(df$horsepower)
+hist(df$Lhorsepower)
+
+mod03 = lm(Lprice~Lhorsepower, data = df)
+summary(mod03)
+
+plot(df$Lhorsepower, df$Lprice)
+abline(coef(mod03))
+
+#residual
+e1 <- resid(mod03)
+round(e1,3) 
+plot(mod1$fitted,e1, pch=16)
+abline(h=0, col=2, lty=2) 
+
+#Shapiro
+shapiro.test(mod1$resid)
+shapiro.test(mod02$resid)
+shapiro.test(mod03$resid)
+
+#resultados
+#mod 01: Yhat = -4630.70 + 173.13 * horsepower
+#mod 02: log(Lprice) = 8.187207 + 0.0112502 * horsepower
+#mod 03: log(Lprice) = 3.55712 + 1.26534 * logHorsepower
+
+Yhat1 = -4630.70 + 173.13 * 127; Yhat1 # Previsao modelo 01
+Yhat2 = (8.187207) + + 0.0112502 * 127; exp(Yhat2) # Previsao modelo 02
+Yhat3 = 3.55712 + 1.26534 * (log(127)); exp(Yhat3) # Previsao modelo 03
+
+  
