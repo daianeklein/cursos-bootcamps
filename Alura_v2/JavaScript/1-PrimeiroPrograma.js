81d19970160a456a5654e866a15739508e82cfda
daianeklein <daiane.klein22@gmail.com>
2022-03-31 17:25:26 -0300

    add js folder

---
+++ Alura_v2/JavaScript/1-PrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+console.log("Trabalhando com variáveis")
+
+const idade = 26;
+
+console.log(idade)
+console.log(idade + 2)
+
+const idadeSomada = idade + 2
+console.log(idadeSomada)
