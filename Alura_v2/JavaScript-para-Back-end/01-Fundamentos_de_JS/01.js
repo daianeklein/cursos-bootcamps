5d01956bdaa1f867918c47ca5936ed6d247853de
daianeklein <daiane.klein22@gmail.com>
2022-04-13 22:26:39 -0300

    add js folder

---
+++ Alura_v2/JavaScript-para-Back-end/01-Fundamentos_de_JS/01.js
@@ -0,0 +1,18 @@
+// Operador ternário
+
+const idadeMinima =  18;
+const idadeCliente = 17
+
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco")
+
+//Template - Strings
+const nome = "Daiane"
+const idade = 2022 - 1999
+const cidadeDeNascimento = "Recife"
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento} `
+console.log(apresentacao)
+
+//comparacao
+const var1 = ''
+console.log(var1 == 0)
