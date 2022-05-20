5704370d25bfd535e33dc0fa977bb3a78180fdd1
daianeklein <daiane.klein22@gmail.com>
2022-05-20 19:02:33 -0300

    organizing files and starting customer analytics

---
+++ Customer-analytics/new-notebook/python/customer-analytics.py
@@ -0,0 +1,83 @@
+# Databricks notebook source
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# COMMAND ----------
+
+# MAGIC %md # CUSTOMER ANALYTICS
+
+# COMMAND ----------
+
+
+# - Master beginner and advanced customer analytics
+# 
+# - Learn the most important type of analysis applied by mid and large companies
+# 
+# - Gain access to a professional team of trainers with exceptional quant skills
+# 
+# - Wow interviewers by acquiring a highly desired skill
+# 
+# - Understand the fundamental marketing modeling theory: segmentation, targeting, positioning, marketing mix, and price elasticity;
+# 
+# - Apply segmentation on your customers, starting from raw data and reaching final customer segments;
+# 
+# - Perform K-means clustering with a customer analytics focus;
+# 
+# - Apply Principal Components Analysis (PCA) on your data to preprocess your features;
+# 
+# - Combine PCA and K-means for even more professional customer segmentation;
+# 
+# - Deploy your models on a different dataset;
+# 
+# - Learn how to model purchase incidence through probability of purchase elasticity;
+# 
+# - Model brand choice by exploring own-price and cross-price elasticity;
+# 
+# - Complete the purchasing cycle by predicting purchase quantity elasticity
+# 
+# - Carry out a black box deep learning model with TensorFlow 2.0 to predict purchasing behavior with unparalleled accuracy
+# 
+# - Be able to optimize your neural networks to enhance results
+
+
+# COMMAND ----------
+
+# MAGIC %md # Libs
+
+# COMMAND ----------
+
+
+# In[ ]:
+
+
+
+
+
+
+# COMMAND ----------
+
+# MAGIC %md # Dataset
+
+# COMMAND ----------
+
+
+# In[ ]:
+
+
+
+
+
+
+# COMMAND ----------
+
+# MAGIC %md # Analysis
+
+# COMMAND ----------
+
+
+# In[ ]:
+
+
+
+
